52d71a60e1cc6f9226d57d3eaa974d2ca66a32ba
julienPardoel <julien.pardoel@gmail.com>
2021-02-24 15:42:02 +0100

    data incorporées

---
+++ src/components/Articles.js
@@ -0,0 +1,21 @@
+const Articles = ({datas}) => {
+    return (
+        <>
+            {datas.map((post) => {
+                const { id, title, article, hashtag } = post;
+                return (
+                    <div className="articles" key="id">
+                        <article>
+                            <h2>{title}</h2>
+                            <p>{article}</p>
+                            <br />
+                            <a>{hashtag}</a>
+                        </article>
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+export default Articles
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, {useState}from 'react';
 import Navigation from '../components/Navigation';
 import Formulaire from '../components/Formulaire';
+import Articles from '../components/Articles';
+import data from '../data/data';
 
 const Blog = () => {
+    const[datas] = useState(data);
     return (
         <div className="blog">
             <Navigation />
             <h1>Blog</h1>
             <Formulaire />
-            <article>
-                <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi necessitatibus molestiae alias cum. Dicta, nihil eum temporibus voluptate consequatur illum. Cum illum voluptatem perferendis aliquam tempora enim molestiae, sapiente maiores? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore doloribus consequatur iusto mollitia. Amet excepturi commodi nemo dicta omnis ipsum nulla accusantium fuga dignissimos ratione! Odit nam obcaecati expedita harum.
-                    <br/>
-                    <a>#Hashtag</a>
-                </p>
-            </article>
+            <Articles datas={datas} />
         </div>
     )
 }
