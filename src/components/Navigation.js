458dec3cb2cd95a0dc0690709f3f900902ad4450
julienPardoel <julien.pardoel@gmail.com>
2021-02-25 16:14:40 +0100

    add mediaqueries

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,18 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
     return (
         <div className="navigation">
-            <NavLink exact to="/" activeClassName="nav-active">Home</NavLink>
-            <NavLink exact to="/blog" activeClassName="nav-active">Blog</NavLink>
-            <NavLink exact to="/contact" activeClassName="nav-active">Contact</NavLink>
+            <div className="desk">
+                <NavLink exact to="/" activeClassName="nav-active">Home</NavLink>
+                <NavLink exact to="/blog" activeClassName="nav-active">Blog</NavLink>
+                <NavLink exact to="/contact" activeClassName="nav-active">Contact</NavLink>
+            </div>
+            <div className="burger">
+                <div className="tranche top"></div>
+                <div className="tranche middle"></div>
+                <div className="tranche bottom"></div>
+            </div>
         </div>
+
     )
 }
 
